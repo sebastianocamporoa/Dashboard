8b328412283715cc0193d038bdb4bad1d4140084
Sebastian Ocampo Roa <sebastian45roa@gmail.com>
2023-07-02 15:01:26 -0500

    [FEAT] add token to get user by id

--- src/services/users/users.js
+++ src/services/users/users.js
@@ -1,5 +1,12 @@
+import Cookies from "js-cookie";
 import { axiosInstance } from "../instance.js";
 
 export const users = () => axiosInstance.get("api/users");
 
-export const user = (id) => axiosInstance.get(`api/users/${id}`);
+export const user = (id) => {
+  return axiosInstance.get(`api/users/${id}`, {
+    headers: {
+      Authorization: Cookies.get("token"),
+    },
+  });
+};
